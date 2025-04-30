bef0f73c743f6bda652a64ab9a13c0db0f4a12df
bejaze <bejaze.hoxha@gmail.com>
2025-04-30 10:03:32 -0500

    Updated the layout, added event listener

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// This array of objects includes the country's name and the path to the country's flag image.
-
+/*----- Country Data -----*/
 const countries = [
     { name: "Albania", flag: "https://flagpedia.net/data/flags/w1160/al.webp" },
     { name: "France", flag: "https://flagpedia.net/data/flags/w1160/fr.webp" },
@@ -8,26 +7,32 @@ const countries = [
     { name: "Colombia", flag: "https://flagpedia.net/data/flags/w1160/co.webp" },
     { name: "Brazil", flag: "https://flagpedia.net/data/flags/w1160/br.webp" }
   ];
-  
-//Created a remainingCountries variable that can be changed later. The ... spread syntax makes a 
-//copy of the countries array and removes countries from the new list without changing the original.
-//I want the countries already used removed from the list as the game progresses. 
+/*----- State Variables -----*/
+  let remainingCountries;
+  let score;
+  let currentCountry;
 
-let remainingCountries = [...countries];
-let score = 0;
-
-
-//Used getElementById to connect my JS code to HTML elements to read or change them.
-
+/*----- cached elements  -----*/
 const flagImg = document.getElementById("flag");
 const optionsContainer = document.getElementById("options-container");
 const feedbackEl = document.getElementById("feedback");
 const scoreEl = document.getElementById("score");
 
+/*----- event listeners -----*/
+restartBtn.addEventListener('click', init);
+
+/*----- functions -----*/
+init();
+
+// Initialize all state and then call render()
+function init() {
+  remainingCountries = [...countries];
+
+  // TODO: Initialize other state variables
+  render();
+}
+  
 
   
 
 
-
-  //Will use the getRandomOptions funtion to create 4 answer choices, with the order being shuffled 
-  // so the correct answer isn't in the same spot always.
