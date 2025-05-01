984ff4a1289a3f3672600348ed6a6b02a3cd0669
bejaze <bejaze.hoxha@gmail.com>
2025-05-01 07:21:25 -0500

    Added splice and checkAnswer Function

--- script.js
+++ script.js
@@ -17,9 +17,18 @@ const flagImg = document.getElementById("flag");
 const optionsContainer = document.getElementById("options-container");
 const feedbackEl = document.getElementById("feedback");
 const scoreEl = document.getElementById("score");
+const restartBtn = document.getElementById("restart-btn");
+const optionBtns = document.querySelectorAll(".option-btn");
 
 /*----- event listeners -----*/
 restartBtn.addEventListener('click', init);
+
+optionBtns.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    const selectedOption = e.target.textContent;
+    checkAnswer(selectedOption, currentCountry.name);
+  });
+});
 
 /*----- functions -----*/
 init();
@@ -33,8 +42,41 @@ function init() {
   flagImg.style.display = 'block';
   render ();
 }
+
+function render() {
+  feedbackEl.textContent = '';
+  optionsContainer.innerHTML = '';
+
+  if (remainingCountries.length === 0) {
+    flagImg.style.display = 'none';
+    feedbackEl.textContent = ` Game Over! Final Score: ${score}`;
+    }
+}
   
+const index = Math.floor(Math.random() * remainingCountries.length);
+currentCountry = remainingCountries [index];
+flagImg.src = currentCountry.flag;
 
-  
+const options = getRandomOptions(currentCountry);
+options.forEach(option => {
+  const btn = document.createElement("button");
+  btn.textContent = option;
+  btn.className = "option-btn";
+  btn.onclick = () => checkAnswer(option, currentCountry.name);
+  optionsContainer.appendChild(btn);
+});
 
+remainingCountries.splice(index,1);
 
+//TODO - add a check answer function
+
+function checkAnswer(selected, correct) {
+  if (selected === correct) {
+    feedbackEl.textContent = "Correct!";
+    score++;
+  } else {
+    feedbackEl.textContent = `Wrong! It was ${correct}`;
+  }
+  scoreEl.textContent = "Score" + score;
+  render();
+}
